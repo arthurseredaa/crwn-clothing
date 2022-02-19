b35967a574b9988ca20d60f3c7623467887e94e0
arthurseredaa <neonbetcy@gmail.com>
2022-02-20 00:34:36 +0200

    Update Button component

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -3,16 +3,5 @@ import styles from './button.module.scss';
 export const Button = ({
   children = 'Please add children to the button',
   className = '',
-  type = '',
-  handleClick,
-  disabled
-}) => (
-  <button
-    type={type}
-    className={`${styles.button} ${className}`}
-    onClick={handleClick}
-    disabled={disabled}
-  >
-    {children}
-  </button>
-);
+  ...otherProps
+}) => <button className={`${styles.button} ${className}`} {...otherProps}>{children}</button>;
